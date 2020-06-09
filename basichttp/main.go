3002a4422341ffa64ea1a1d3f4ccbf49f4b947f6
JerryZhang <me@zhangjiee.com>
2020-06-09 17:57:44 +0800

    t

--- basichttp/main.go
+++ basichttp/main.go
@@ -1,5 +1,16 @@
 package main
 
 func main() {
-	// TODO: start a http server simple logrusredis hook log to redis
+	// http.HandleFunc("/log", func(w http.ResponseWriter, req *http.Request) {
+	// 	defer req.Body.Close()
+
+	// 	bs, err := ioutil.ReadAll(req.Body)
+	// 	if err != nil {
+	// 		log.Printf()
+	// 		w.Write([]byte("ok"))
+	// 	}
+
+	// 	w.Write([]byte("ok"))
+	// })
+	// http.ListenAndServe(":8080", nil)
 }
