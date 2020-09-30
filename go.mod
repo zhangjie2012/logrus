c3f7fdaf3eca2736357f2ec0f6d53afadbd6ba3c
luanhua <me@zhangjiee.com>
2020-09-30 16:09:31 +0800

    refactor

--- go.mod
+++ go.mod
@@ -1,10 +1,12 @@
-module github.com/zhangjie2012/logrusredis
+module github.com/zhangjie2012/logrusredis-hook
 
 go 1.14
 
 require (
 	github.com/go-redis/redis/v7 v7.4.0
 	github.com/onsi/ginkgo v1.12.3 // indirect
-	github.com/prometheus/client_golang v1.6.0
-	github.com/sirupsen/logrus v1.6.0
+	github.com/sirupsen/logrus v1.7.0
+	github.com/stretchr/testify v1.4.0
+	github.com/vmihailenco/msgpack v4.0.4+incompatible
+	google.golang.org/appengine v1.6.5 // indirect
 )
--- logrusredis.go
+++ logrusredis.go
@@ -1,91 +1,57 @@
 package logrusredis
 
 import (
-	"encoding/json"
 	"fmt"
+	"net"
 	"path/filepath"
+	"runtime"
+	"time"
 
 	redis "github.com/go-redis/redis/v7"
 	"github.com/sirupsen/logrus"
+	"github.com/vmihailenco/msgpack"
 )
 
-type Option struct {
+type RedisOption struct {
 	Addr     string
 	Password string
 	DB       int
 	Key      string
-	AppName  string
-	Ip       string
 }
 
+type LogWashFunc func(appName string, t time.Time, metadata logrus.Fields, caller *runtime.Frame, level logrus.Level, message string) []byte
+
 type LogrusRedisHook struct {
-	option     *Option
-	rClient    *redis.Client
-	production bool
+	appName string
+	option  *RedisOption
+	rClient *redis.Client
+	logWash LogWashFunc
 }
 
 func (h *LogrusRedisHook) Levels() []logrus.Level {
-	if h.production {
-		return []logrus.Level{
-			logrus.InfoLevel,
-			logrus.WarnLevel,
-			logrus.ErrorLevel,
-			logrus.FatalLevel,
-			logrus.PanicLevel,
-		}
-	} else {
-		return []logrus.Level{
-			logrus.TraceLevel,
-			logrus.DebugLevel,
-			logrus.InfoLevel,
-			logrus.WarnLevel,
-			logrus.ErrorLevel,
-			logrus.FatalLevel,
-			logrus.PanicLevel,
-		}
+	return []logrus.Level{
+		logrus.TraceLevel,
+		logrus.DebugLevel,
+		logrus.InfoLevel,
+		logrus.WarnLevel,
+		logrus.ErrorLevel,
+		logrus.FatalLevel,
+		logrus.PanicLevel,
 	}
 }
 
-type LogS struct {
-	Timestamp int64         `json:"@timestamp"`
-	AppName   string        `json:"@appname"`
-	MetaData  logrus.Fields `json:"@metadata"` // map[string]interface{}
-	Ip        string        `json:"@ip"`
-	Level     string        `json:"@level"`
-	Caller    string        `json:"@caller"`
-	Msg       string        `json:"@msg"`
+func (h *LogrusRedisHook) Fire(e *logrus.Entry) (err error) {
+	bs := h.logWash(h.appName, e.Time, e.Data, e.Caller, e.Level, e.Message)
+	if bs == nil {
+		// ignore logs
+		return nil
+	}
+
+	_, err = h.rClient.LPush(h.option.Key, bs).Result()
+	return
 }
 
-func (h *LogrusRedisHook) Fire(e *logrus.Entry) error {
-	caller := ""
-	if e.Caller != nil {
-		caller = fmt.Sprintf("%s:%d", filepath.Base(e.Caller.File), e.Caller.Line)
-	}
-
-	log := LogS{
-		Timestamp: e.Time.UnixNano(),
-		AppName:   h.option.AppName,
-		MetaData:  e.Data,
-		Ip:        h.option.Ip,
-		Level:     e.Level.String(),
-		Caller:    caller,
-		Msg:       e.Message,
-	}
-
-	logBs, err := json.Marshal(log)
-	if err != nil {
-		return err
-	}
-
-	_, err = h.rClient.LPush(h.option.Key, logBs).Result()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func NewHook(option *Option, production bool) (*LogrusRedisHook, error) {
+func NewHook(appName string, option *RedisOption, logWashFunc LogWashFunc) (*LogrusRedisHook, error) {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     option.Addr,
 		Password: option.Password,
@@ -94,9 +60,77 @@ func NewHook(option *Option, production bool) (*LogrusRedisHook, error) {
 	if _, err := rdb.Ping().Result(); err != nil {
 		return nil, err
 	}
+	if logWashFunc == nil {
+		logWashFunc = DefaultLogWashFunc
+	}
 	return &LogrusRedisHook{
-		rClient:    rdb,
-		option:     option,
-		production: production,
+		appName: appName,
+		rClient: rdb,
+		option:  option,
+		logWash: logWashFunc,
 	}, nil
+}
+
+// -----------------------------------------------------------------------------
+
+type DefaultLogS struct {
+	Timestamp int64         `msgpack:"@timestamp"`
+	MetaData  logrus.Fields `msgpack:"@metadata"` // map[string]interface{}
+	Ip        string        `msgpack:"@ip"`
+	Level     string        `msgpack:"@level"`
+	Caller    string        `msgpack:"@caller"`
+	Message   string        `msgpack:"@message"`
+}
+
+// https://stackoverflow.com/a/37382208/802815
+func getOutboundIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return ""
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP.String()
+}
+
+var instanceIp string
+
+func getIp() string {
+	if instanceIp == "" {
+		instanceIp = getOutboundIP()
+	}
+	return instanceIp
+}
+
+// DefaultLogWashFunc log filter and serialization
+//   - use msgpack serialize log
+//   - level > 'DEBUG'
+//   - set current instance ip
+func DefaultLogWashFunc(appName string, t time.Time, metadata logrus.Fields, caller *runtime.Frame, level logrus.Level, message string) []byte {
+	if level > logrus.DebugLevel {
+		return nil
+	}
+
+	caller_ := ""
+	if caller != nil {
+		caller_ = fmt.Sprintf("%s:%d", filepath.Base(caller.File), caller.Line)
+	}
+
+	l := DefaultLogS{
+		Timestamp: t.UnixNano(),
+		MetaData:  metadata,
+		Ip:        getIp(),
+		Level:     level.String(),
+		Caller:    caller_,
+		Message:   message,
+	}
+
+	bs, err := msgpack.Marshal(&l)
+	if err != nil {
+		return nil
+	}
+
+	return bs
 }
